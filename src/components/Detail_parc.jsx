47af01ff08d407cfbb15d6b5eed04d2c66e2ef32
Adeline <149504531+kna1703@users.noreply.github.com>
2024-05-16 20:42:33 +0200

    parc detail

--- src/components/Detail_parc.jsx
+++ src/components/Detail_parc.jsx
@@ -1,3 +1,28 @@
-export default function Detail_parc() {
-  return <div>Je suis Detail_parc</div>;
+import Card from 'react-bootstrap/Card';
+import { Container } from "react-bootstrap";
+
+function Detail_parc() {
+  return (
+    <>
+    <Container>
+      <Card>
+          <Card.Body style={{display: "flex", flexDirection: "column", alignItems:"center", backgroundColor: "#F2F0EC"}}>
+            <Card.Title> <h2>Détails du parc-jardin</h2></Card.Title>
+              <Card.Text>
+                <ul>
+                  <li style={{listUnstyled:"none"}}>Pollution sonore</li>
+                  <li>Pollution de l'air</li>
+                  <li>Couvert végétal</li>
+                </ul> 
+              </Card.Text>
+            </Card.Body>
+    </Card>
+  </Container>
+  </>
+);
 }
+
+
+
+
+export default Detail_parc
