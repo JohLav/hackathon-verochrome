8356523835d11c8e8a5a7f0fe2d7398e62813ed9
Johana Lavigne <johana.lavigne@gmail.com>
2024-05-16 11:53:44 +0200

    Bootstrap setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import Button from "react-bootstrap/Button";
+import Map from "./components/Map.jsx";
+import { Container } from "react-bootstrap";
+import './App.scss';
 
+
+function App() {
   return (
     <>
+      <Container>
+        <Map />
+        <Button>Checkout</Button>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,14 @@
+import Figure from "react-bootstrap/Figure";
+
+export default function Map() {
+  return (
+    <>
+      <Figure.Image
+        width={171}
+        height={180}
+        alt="171x180"
+        src="src/assets/Capture-ARemplacer.png"
+      />
+    </>
+  );
+}
