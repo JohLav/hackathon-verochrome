469b395d35c1ffaf0a3cf6c1ccdb9cc6d0116094
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-16 17:29:37 +0200

    Info_parc component

--- src/components/Info_parc.jsx
+++ src/components/Info_parc.jsx
@@ -1,3 +1,22 @@
+import imgPage from "../assets/fourviere.jpg";
+
 export default function Info_parc() {
-  return <div>Je suis Info_parc</div>;
+  return (
+    <>
+      <div
+        className=" d-flex justify-content-center align-items-center bg-info w-50 rounded-1 mt-5 "
+        style={{ margin: "0 auto ", color: "#614544" }}
+      >
+        <h1>Parc de la tête d&apos; Or</h1>
+      </div>
+      <div className="d-flex justify-content-center py-2 ">
+        <img
+          src={imgPage}
+          alt="basilique"
+          className="w-50 rounded-1"
+          style={{ margin: "0 auto " }}
+        />
+      </div>
+    </>
+  );
 }
