dab07bbcb3782bc5ec546da40dc43846c6eb9732
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-17 10:05:41 +0200

    Info_Parc component with useLocation

--- src/components/Info_parc.jsx
+++ src/components/Info_parc.jsx
@@ -1,13 +1,16 @@
+import { useLocation } from "react-router-dom";
 import imgPage from "../assets/fourviere.jpg";
 
 export default function Info_parc() {
+  const location = useLocation();
+  const { parcData } = location.state;
   return (
     <>
       <div
         className=" d-flex justify-content-center align-items-center bg-info w-50 rounded-1 mt-5 "
         style={{ margin: "0 auto ", color: "#614544" }}
       >
-        <h1>Parc de la tête d&apos; Or</h1>
+        <h1>{parcData.nom}</h1>
       </div>
       <div className="d-flex justify-content-center py-2 ">
         <img
