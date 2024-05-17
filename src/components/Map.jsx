aefa3adcfcd47a6fe791861c0cc449a7ddd8583c
Johana Lavigne <johana.lavigne@gmail.com>
2024-05-17 12:10:23 +0200

    Updated map link without white space

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,14 +4,12 @@ export default function Map() {
   const ref = useRef(null);
 
   const url =
-    "https://public.tableau.com/views/hackathon2_17159384045180/Tableaudebord1?:language=fr-FR&publish=yes&:sid=&:display_count=n&:origin=viz_share_link";
+    "https://public.tableau.com/views/hackathon4_17159405552530/Tableaudebord1?:language=fr-FR&publish=yes&:sid=&:display_count=n&:origin=viz_share_link";
 
   useEffect(() => {
     const { tableau } = window;
 
     const options = {
-      width: "100%",
-      height: "600px",
       hideTabs: true,
       onFirstInteractive: function () {
         console.log("Tableau Viz has finished loading");
@@ -30,7 +28,7 @@ export default function Map() {
 
   return (
     <>
-      <div ref={ref}></div>
+      <div className="tableau" ref={ref}></div>
     </>
   );
 }
